16881eb899bfb31bbbeecb9668fd70c70afcebce
georgi.georgiev <ggeorgiev83@gmail.com>
2022-03-25 11:35:24 +0200

    1st

---
+++ src/main/java/com/CommandLineRunner.java
@@ -0,0 +1,15 @@
+package com;
+
+
+
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+
+public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/com/xml/XmlIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
